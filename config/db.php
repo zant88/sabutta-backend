a913aae8d835cbd45abdb3d871c3987546aac901
Ehsan Basri <ehsan.basri@djkonsultan.com>
2022-04-13 23:04:51 +0700

    [CHANGE] Revert to db docker connection

--- config/db.php
+++ config/db.php
@@ -1,17 +1,17 @@
 <?php
 
 return [
-    // 'class' => 'yii\db\Connection',
-    // 'dsn' => 'mysql:host=db;dbname=pupr',
-    // 'username' => 'root',
-    // 'password' => 'Fasyankes!@#',
-    // 'charset' => 'utf8',
-    
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=172.17.0.2;dbname=pupr',
+    'dsn' => 'mysql:host=db;dbname=pupr',
     'username' => 'root',
-    'password' => 'PassYankes2021!',
+    'password' => 'Fasyankes!@#',
     'charset' => 'utf8',
+    
+    // 'class' => 'yii\db\Connection',
+    // 'dsn' => 'mysql:host=172.17.0.2;dbname=pupr',
+    // 'username' => 'root',
+    // 'password' => 'PassYankes2021!',
+    // 'charset' => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
